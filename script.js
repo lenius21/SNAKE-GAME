765b2820891a2146c1695415b34bcbe21ea20859
lenius21 <cheng_chi_kiang@nass.moe.edu.sg>
2026-07-01 20:28:16 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+body{
+    margin:0;
+    background:#111;
+    color:white;
+    font-family:Arial,Helvetica,sans-serif;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+}
+
+h1{
+    margin-top:20px;
+}
+
+canvas{
+    background:#222;
+    border:4px solid white;
+    margin-top:20px;
+}
+
+.info{
+    display:flex;
+    gap:30px;
+    align-items:center;
+    margin-top:10px;
+}
+
+button{
+    padding:10px 20px;
+    cursor:pointer;
+    border:none;
+    border-radius:6px;
+    font-size:16px;
+}
+
+button:hover{
+    background:#ddd;
+}
